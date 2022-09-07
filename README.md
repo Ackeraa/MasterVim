760a966c630cbe00a27568dee31dee6ca334ff29
Ackeraa <ggaoshanxun@gmail.com>
2022-09-08 03:37:33 +0800

    added dev info

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # master-vim
+
+# from 0 to 1
+
+## Attributes
+* HP
+* ATK
+* money
+* keys 
+* weapon
+* magic
+
+## Movements
+Normal Mode:
+* h, l, j, k, w, e, b, 0, $ -> basic move
+* d* -> destroy
+* x -> pick up, hold in hand
+* p -> use tool in hand
+* "[0-9]x -> pick up, put into bag[0-9]
+* "[0-9]p -> use tool in bag[0-9]
+
+Insert Mode:
+* 
+
+Visual Mode:
+*
+
+Command Mode:
+*
+
+
+## Ideas
+* Try to keep as simple as possible.
+* Encourage fast move by introducing dangerous cells.
+* Could use multi-cell as one to imporve UI.
+* Get move skills gradually.
+
+
--- lua/master-vim/utils.lua
+++ lua/master-vim/utils.lua
@@ -1,1 +1,25 @@
-print("This is utils file")
+local utils = {}
+
+utils.create_bufwin = function(width, height, row, col, border, zindex)
+
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+    vim.api.nvim_buf_set_option(buf, "filetype", "todo")
+
+    local opts = {
+        style = "minimal",
+        relative = "editor",
+        width = width,
+        height = height,
+        row = row,
+        col = col,
+        border = border,
+        zindex = zindex,
+    }
+
+    local win_id = vim.api.nvim_open_win(buf, false, opts)
+
+    return buf, win_id
+end
+
+return utils
